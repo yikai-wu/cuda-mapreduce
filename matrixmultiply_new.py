bbbc2ab48ebe5b9aa482e2617a5deedb4e311639
Yikai Wu <31660663+yikai-wu@users.noreply.github.com>
2024-05-05 21:39:48 -0400

    Update matrixmultiply

--- matrixmultiply_new.py
+++ matrixmultiply_new.py
@@ -10,48 +10,79 @@ def matrix_multiply_cpu(A, B):
     cpu_time=time.time()-cpu_start
     return C, cpu_time
 
-TPB = 32
 
 @cuda.jit
-def gpu_matrix_mult(A, B, C):
-    """ Perform matrix multiplication of C = A * B """
+def fast_matmul(A, B, C):
+    TPB = 16
+
+    sA = cuda.shared.array(shape=(TPB, TPB), dtype=float32)
+    sB = cuda.shared.array(shape=(TPB, TPB), dtype=float32)
+
     x, y = cuda.grid(2)
-    if x >= C.shape[0] or y >= C.shape[1]:
+
+    tx = cuda.threadIdx.x
+    ty = cuda.threadIdx.y
+    bpg = cuda.gridDim.x    
+
+    if x >= C.shape[0] and y >= C.shape[1]:
         return
 
-    tmp = float(0)
-    for k in range(A.shape[1]):
-        tmp += A[x, k] * B[k, y]
+    tmp = 0.
+    for i in range(bpg):
+        sA[tx, ty] = A[x, ty + i * TPB]
+        sB[tx, ty] = B[tx + i * TPB, y]
+
+        cuda.syncthreads()
+
+        for j in range(TPB):
+            tmp += sA[tx, j] * sB[j, ty]
+
+        cuda.syncthreads()
 
     C[x, y] = tmp
 
 def matrix_multiply_gpu(A, B):
-    """ Host code to setup the computation on the GPU """
-    start_time=time.time()
-    stream1 = cuda.stream()
-    stream2 = cuda.stream()
-    stream3 = cuda.stream()
+    TPB = 16
+    n, k = A.shape
+    k, m = B.shape
+    start = time.time()
+    A_device = cuda.to_device(A)
+    B_device = cuda.to_device(B)
+    C_device = cuda.device_array((n, m), dtype=np.float32)
 
-    A_global_mem = cuda.to_device(A, stream=stream1)
-    B_global_mem = cuda.to_device(B, stream=stream2)
-    C_global_mem = cuda.device_array((A.shape[0], B.shape[1]), dtype=np.float32)
+    threads_per_block = (TPB, TPB)
+    blocks_per_grid_x = math.ceil(m / TPB)
+    blocks_per_grid_y = math.ceil(n / TPB)
 
-    threadsperblock = (TPB, TPB)
-    blockspergrid_x = math.ceil(A.shape[0] / TPB)
-    blockspergrid_y = math.ceil(B.shape[1] / TPB)
-    blockspergrid = (blockspergrid_x, blockspergrid_y)
+    cuda.synchronize()
+    kernel_start_time = time.time()
+    fast_matmul[(blocks_per_grid_y, blocks_per_grid_x), threads_per_block](A_device, B_device, C_device)
+    cuda.synchronize()  # Wait for all GPU activity to finish
+    kernel_time = time.time()-kernel_start_time
 
-    kernel_start_time=time.time()
-    gpu_matrix_mult[blockspergrid, threadsperblock, stream3](A_global_mem, B_global_mem, C_global_mem)
-    kernel_time=time.time()-kernel_start_time
+    C = C_device.copy_to_host()
+    gpu_time = time.time() - start
+    return C, kernel_time, gpu_time
 
-    # Wait for all streams to complete their tasks
-    stream1.synchronize()
-    stream2.synchronize()
-    stream3.synchronize()
+def random_matrix(n):
+    A = np.random.default_rng().standard_normal(size=(n,n), dtype='float32')
+    B = np.random.default_rng().standard_normal(size=(n,n), dtype='float32')
+    return A, B
 
-    C = C_global_mem.copy_to_host()
-    gpu_time = time.time()-start_time
-    return C, gpu_time, kernel_time
+A, B = random_matrix(10000)
+print(A.shape)
+print(B.shape)
+
+cpu_start=time.time()
+total_kernel_time=0.0
+C_cpu, cpu_time = matrix_multiply_cpu(A, B)
+total_cpu_time=time.time()-cpu_start
+gpu_start=time.time()
+C, kernel_time, gpu_time =  matrix_multiply_gpu(A, B)
+total_gpu_time=time.time()-gpu_start
+print(C_cpu)
+print(C)
+print(np.sum(C-C_cpu))
+print(f"CPU time: {total_cpu_time}s, GPU time: {total_gpu_time}s, kernel time: {kernel_time}s")
 
 
